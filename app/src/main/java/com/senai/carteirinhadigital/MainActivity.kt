9bb9924ea96d15ead8411839ca51c448fbd839e6
suy <amanda.baroboskin@gmail.com>
2026-03-05 16:45:16 -0300

    dfghjk

--- app/src/main/java/com/senai/carteirinhadigital/MainActivity.kt
+++ app/src/main/java/com/senai/carteirinhadigital/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.senai.carteirinhadigital.ui.theme.CarteirinhaDigitalTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +25,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CarteirinhaDigitalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "caçadora de ratos profissional",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    innerPadding ->
+                    CarteirinhaDigitalApp(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
                     )
                 }
             }
@@ -31,17 +39,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Olá $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CarteirinhaDigitalTheme {
-        Greeting("Android")
+fun CarteirinhaDigitalApp(modifier: Modifier = Modifier) {
+    Column(
+        modifier=modifier,
+      horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(space = 40.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.senai_logo),
+            contentDescription = "Senai logo preta"
+        )
+        Image(
+            painter = painterResource(R.drawable.primobigode),
+            contentDescription = "Primo bigode"
+        )
     }
 }
