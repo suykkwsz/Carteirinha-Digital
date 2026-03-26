15e4abdbc317921bd7f282ac18592e147d991b6b
suykkwsz <amanda.baroboskin@gmail.com>
2026-03-26 16:55:44 -0300

    abu

--- app/src/main/java/com/senai/carteirinhadigital/MainActivity.kt
+++ app/src/main/java/com/senai/carteirinhadigital/MainActivity.kt
@@ -5,18 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.rafaelcosta.carteirinhadigital_4devm_t1.feature.carteirinha.presentation.component.QrCode
 import com.senai.carteirinhadigital.ui.theme.CarteirinhaDigitalTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,18 +52,70 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CarteirinhaDigitalApp(modifier: Modifier = Modifier) {
-    Column(
-        modifier=modifier,
-      horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(space = 40.dp)
-    ) {
+    Box {
         Image(
-            painter = painterResource(R.drawable.senai_logo),
-            contentDescription = "Senai logo preta"
+            painter = painterResource(id = R.drawable.fundoroxo),
+            contentDescription = "fundo",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
         )
-        Image(
-            painter = painterResource(R.drawable.primobigode),
-            contentDescription = "Primo bigode"
+        Column(
+            modifier = modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(space = 10.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.senai_logo),
+                contentDescription = "Senai logo preta",
+                modifier = Modifier
+                    .size(250.dp)
+            )
+            Image(
+                painter = painterResource(R.drawable.eu),
+                contentDescription = "Primo bigode",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(CircleShape)
+                    .border(width = 2.dp, color = Color.Blue, CircleShape)
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(
+                    text = "Nome: ",
+                    fontSize = 15.sp
+                )
+                Text(
+                    text = "Amanda Campillo",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(text = "Curso: ")
+                Text(text = "Desenvolvimento de Sistemas")
+            }
+
+            QrCode(conteudo = "90000000001755008306")
+        }
+    }
+}
+
+@Preview(
+    showSystemUi = true,
+    showBackground = true
+)
+@Composable
+fun CarteirinhaDigitalAppPreview() {
+    CarteirinhaDigitalTheme {
+        CarteirinhaDigitalApp(
+            modifier = Modifier.fillMaxSize()
         )
     }
 }
